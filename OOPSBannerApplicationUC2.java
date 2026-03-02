c14958665823d1c69f7514963ace99ee815def3a
kk0434-wq <kk0434@srmist.edu.in>
2026-03-02 13:26:53 +0530

    Codded to print OOPS using println

---
+++ OOPSBannerApplicationUC2.java
@@ -0,0 +1,11 @@
+public class OOPSBannerApplicationUC2 {
+    public static void main(String[] args) {
+        System.out.println("    ***    " + "    ***     " + "******   "+"   ***** ");
+        System.out.println(" **     ** " + " **     **  " + "**    ** "+" **      ");
+        System.out.println("**       **" + "**       ** " + "**    ** "+"**       ");
+        System.out.println("**       **" + "**       ** " + "***** ** "+"  *****  ");
+        System.out.println("**       **" + "**       ** " + "**       "+"      ** ");
+        System.out.println(" **     ** " + " **     **  " + "**       "+"      ** ");
+        System.out.println("    ***    " + "    ***     " + "**       "+"  ***** "); 
+    }
+}
